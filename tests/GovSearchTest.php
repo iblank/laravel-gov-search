cb8f8462b8e454b39bfe017da4c8a4d477111e1e
Ian Collins <iancollins@Ians-iMac.local>
2015-04-15 14:11:00 -0400

    updated unittests, using mock search class

--- tests/GovSearchTest.php
+++ tests/GovSearchTest.php
@@ -4,6 +4,27 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use iblank\GovSearch\GovSearch;
 
+class MockGovSearch {
+	public function search($search, $offset = 0, $opts = array()) {
+		$limit = (isset($opts['limit'])) ? $opts['limit'] : 20;
+		$results = array();
+		for($i = 0; $i < $limit; $i++){
+			$tempObj = (object) array(
+				"title" => "title #".$i
+			);
+			array_push($results, $tempObj);
+		}
+		$fakeReturn = (object) array(
+			"total" => 234,
+			"next_offset" => $limit,
+			"results" => $results
+		);
+
+		return $fakeReturn;
+	}
+
+}
+
 class GovSearchTest extends \PHPUnit_Framework_TestCase {
 	/**
 	 *
@@ -16,6 +37,7 @@ class GovSearchTest extends \PHPUnit_Framework_TestCase {
 		$TEST_API_KEY = 'weioevinweoih4oi4n24lrn4567'; // jibberish
 		$TEST_API_AFFILIATE = 'www.anything.com';
 		$this->govsearch = new GovSearch( $TEST_API_KEY, $TEST_API_AFFILIATE );
+		$this->mocksearch = new MockGovSearch();
 	}
 
 	public function tearDown() {
@@ -45,7 +67,7 @@ class GovSearchTest extends \PHPUnit_Framework_TestCase {
 		$opts = array(
 			'limit' => rand( 3, 10 )
 		);
-		$response = $this->govsearch->search( 'medal', 0, $opts );
+		$response = $this->mocksearch->search( 'medal', 0, $opts );
 		$this->assertEquals( $opts['limit'], count( $response->results ) );
 	}
 }
