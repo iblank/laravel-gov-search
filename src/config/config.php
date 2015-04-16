63e8ecb17a66fcef9bcc992e53f46bee5e389966
Ian Collins <iancollins@Ians-iMac.local>
2015-04-16 15:21:49 -0400

    added Bing and Google API key integration to the config files

--- src/config/config.php
+++ src/config/config.php
@@ -10,5 +10,12 @@ return array(
         'highlight' => true, // enable keyword highlighting markup within results
         'limit' => 20, // 1 to 999 results
         'sort' => 'relevance' // 'relevance' or 'date'
+    ),
+    'BING' => array( // optional tie-in
+        'KEY' => 'YOUR BING/AZURE API KEY'
+    ),
+    'GOOGLE' => array( // optional tie-in
+        'KEY' => 'YOUR GOOGLE SITE SEARCH API KEY',
+        'CX' => 'YOUR GOOGLE CUSTOM SEARCH ENGINE KEY' // KEY and CX are both required, if using this option
     )
 );
